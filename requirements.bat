e93623787dba23de843156de0fab61fbf232a37e
antiskidDeath <107080345+antiskidDeath@users.noreply.github.com>
2022-06-07 17:02:45 -0500

    Create requirements.bat

---
+++ requirements.bat
@@ -0,0 +1,11 @@
+echo MAKE SURE PYTHON 3.9 IS IN PATH
+
+pip install discord_webhook
+pip install pyinstaller 
+pip install requests
+pip install pypiwin32
+pip install Pillow
+pip install pycryptodomex
+pip install psutil
+
+echo Installed Requirements
